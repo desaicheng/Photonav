b486c085a399e2a7dbcd91dfb9c8e767d4120690
Desai Cheng <desaicheng@Desais-Air.home>
2020-03-14 11:44:20 -0400

    testing

--- Photonav/wsgi.py
+++ Photonav/wsgi.py
@@ -14,4 +14,5 @@ from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Photonav.settings')
 
-application = DjangoWhiteNoise(get_wsgi_application())
+# application = DjangoWhiteNoise(get_wsgi_application())
+application = get_wsgi_application()
