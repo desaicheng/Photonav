9aa877ea88d33f1be4cea279a29559f515b7c87c
Desai Cheng <desaicheng@Desais-Air.home>
2020-03-18 09:27:03 -0400

    testing if heroku works

--- Photonav/wsgi.py
+++ Photonav/wsgi.py
@@ -13,6 +13,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Photonav.settings')
-
-application = DjangoWhiteNoise(get_wsgi_application())
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+print(application)
 # application = get_wsgi_application()
