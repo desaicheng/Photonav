d5ef76aa12cf077c299ba121f13ee7a28129cb5e
Desai Cheng <desaicheng@Desais-Air.home>
2020-03-22 17:17:55 -0400

    debugging sort

--- Home/views.py
+++ Home/views.py
@@ -156,6 +156,7 @@ def sort(request):
 def sortBy(request):
     if request.is_ajax():
         user.sortType = request.GET.get('type', None)
+        print(user.sortType)
         # sort(request)
         # return HttpResponse(json.dumps(user.data[:user.paginationNumber]))
         return HttpResponse(json.dumps({}))
